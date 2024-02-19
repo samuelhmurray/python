96eb318afaea05f38e71a3c94dd202e04e5a8fb8
Sam Murray <shm31094@gmail.com>
2024-02-19 16:56:23 -0500

    left off at Practice: English Idioms

---
+++ dictionaries/dictionaryOfWords.py
@@ -0,0 +1,23 @@
+word_definitions = dict()
+word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+word_definitions["Serendipity"] = "The occurrence of fortunate events by chance"
+word_definitions["Ephemeral"] = "Lasting for a very short time; fleeting"
+word_definitions["Pernicious"] = "Having a harmful effect, especially in a gradual way"
+
+
+"""
+Use square bracket lookup to get the definition of two
+words and output them to the console with `print()`
+"""
+print(word_definitions["Serendipity"])
+print("Definition of 'Pernicious':", word_definitions["Pernicious"])
+
+
+"""
+Loop over the dictionary to get the following output:
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+"""
+for key, value in word_definitions.items():
+    print(f" The definition of {key} is {value}")
